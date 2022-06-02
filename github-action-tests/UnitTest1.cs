572347e4746a4d90cacdf6be2c685c81476fed82
antruong3007 <antruong3007@gmail.com>
2022-06-02 15:36:49 +0700

    add tests

---
+++ github-action-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace github_action_tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(true);
+    }
+}
